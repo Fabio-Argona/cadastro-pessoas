ab2e78330deca5520d1ae08c699b9587c20a07a0
Fabio Argona <128233610+Fabio-Argona@users.noreply.github.com>
2025-05-29 18:21:20 -0300

    feat: setup incial

---
+++ src/test/java/com/condominio/cadastro_pessoas/CadastroPessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.condominio.cadastro_pessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroPessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
